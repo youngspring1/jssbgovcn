794917d6f69ce974e2fd52933a646373a0952f46
youngspring1 <youngspring1@163.com>
2017-06-26 21:33:04 +0800

    02.getElectricity.py

---
+++ 02.getElectricity.py
@@ -0,0 +1,53 @@
+#!/usr/bin/pythonw
+# -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+import urllib.request
+import csv
+
+
+year = '2016'
+month = 2
+urllist = []
+# read csv,get url
+
+with open(year + 'contents.txt', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in spamreader:
+        if row[0] == '全社会用电量指标':
+            urllist = row[2:]
+        
+#print(','.join(urllist))
+
+for target_url in urllist:
+    page = []
+
+    data = urllib.request.urlopen(target_url)
+    datastr = data.read().decode(encoding="utf-8")
+    soup = BeautifulSoup(datastr, 'lxml')
+    table = soup.find(class_ = 'MsoNormalTable')
+    tbody = table.tbody.contents
+    #print(tbody)
+
+    #parse data
+    for tr in tbody:
+        if tr != ' ':
+            tdlist = tr.contents
+            line = []
+            for td in tdlist:
+                if td != ' ':
+                    cell = ''
+                    for result in td.find_all('span'):
+                        if str(result.string) != 'None' and str(result.string) != '#':
+                            cell += str(result.string).replace(' ', '').replace('　', '').strip()
+                    line.append(cell)
+            #print(' : '.join(line))
+            page.append(line)
+
+    # output
+    with open(year + "_" + str(month) + ".csv", "w",newline="") as datacsv:
+        csvwriter = csv.writer(datacsv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for line in page:
+            csvwriter.writerow(line)
+
+    month += 1
