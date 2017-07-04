955e54d682f8a92fb10218d1959184472bbb222f
youngspring1 <youngspring1@163.com>
2017-07-04 21:10:02 +0800

    simplicify

--- crawler/01.getURLList.py
+++ crawler/01.getURLList.py
@@ -3,45 +3,43 @@
 
 from bs4 import BeautifulSoup
 import urllib.request
-import gzip
 
-year = '2016'
-target_url = 'http://www.jssb.gov.cn/tjxxgk/tjsj/jdsj/' + year
-data = urllib.request.urlopen(target_url)
-datastr = data.read().decode(encoding='utf-8')
-#print(datastr)
+def main():
+    year = '2016'
+    target_url = 'http://www.jssb.gov.cn/tjxxgk/tjsj/jdsj/' + year
+    data = urllib.request.urlopen(target_url)
+    datastr = data.read().decode(encoding='utf-8')
+    #print(datastr)
 
+    soup = BeautifulSoup(datastr, 'lxml')
+    #print(soup.title)
+    #print(soup.title.string)
 
-soup = BeautifulSoup(datastr, 'lxml')
-#print(soup.title)
-#print(soup.title.string)
+    #print(type(soup.table.contents))#list
 
-#print(type(soup.table.contents))#list
+    tmplist = soup.table.contents
+    urllist = []
+    for tr in tmplist:
+        if tr != '\n' and tr.name != 'script':
+            #trlist.append(tr)
+            row = []
+            for td in tr.contents:
 
-tmplist = soup.table.contents
-urllist = []
-for tr in tmplist:
-	if tr != '\n' and tr.name != 'script':
-		#trlist.append(tr)
-		row = []
-		for td in tr.contents:
+                if td != '\n' and td.name != 'script':
+                    
+                    if td.find('a'):
+                        row.append(target_url + td.find('a').get('href').replace('.', '', 1))
+                    else:
+                        row.append(td.string)
+            
+            urllist.append(row)
 
-			if td != '\n' and td.name != 'script':
-				
-				if td.find('a'):
-					row.append(url + td.find('a').get('href').replace('.', '', 1))
-				else:
-					row.append(td.string)
-		
-		urllist.append(row)
+    #print(urllist)
 
-#print(urllist)
+    f = open('../data/' + year + '_urllist.csv', 'w')
+    for line in urllist:
+        f.write(','.join(line) + '\n')
+    f.close()
 
-f = open('../data/' + year + '_urllist.csv', 'w')
-for line in urllist:
-    f.write(','.join(line) + '\n')
-f.close()
-
-
-
-
+if __name__ == '__main__':
+    main()
--- crawler/02.getElectricity.py
+++ crawler/02.getElectricity.py
@@ -8,47 +8,100 @@ import csv
 
 year = '2016'
 month = 2 #All data start from Feb.
-urllist = []
+month_name = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+city_name  = ['南京市', '无锡市', '徐州市', '常州市', '苏州市', '南通市', '连云港市', '淮安市', '盐城市', '扬州市', '镇江市', '泰州市', '宿迁市']
 
-# read csv,get url
-with open('../data/' + year + '_urllist.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    for row in spamreader:
-        if row[0] == '全社会用电量指标':
-            urllist = row[2:] #All data start from Feb. Skip row[0] and row[1]
+def main():
+    urllist = []
 
-#print('url num:' + str(len(urllist))) # url num:11
-#print('\n'.join(urllist))
+    # read csv,get url
+    with open('../data/' + year + '_urllist.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in spamreader:
+            if row[0] == '全社会用电量指标':
+                urllist = row[2:] #All data start from Feb. Skip row[0] and row[1]
 
-for target_url in urllist:
-    page = []
+    #print('url num:' + str(len(urllist))) # url num:11
+    #print('\n'.join(urllist))
+    yeardata = []
+    for target_url in urllist:
+        page = []
+        monthdata = []
 
-    data = urllib.request.urlopen(target_url)
-    datastr = data.read().decode(encoding='utf-8')
-    soup = BeautifulSoup(datastr, 'lxml')
-    table = soup.find(class_ = 'MsoNormalTable')
-    tbody = table.tbody.contents
-    #print(tbody)
 
-    #parse data
-    for tr in tbody:
-        if tr != ' ':
-            tdlist = tr.contents
-            line = []
-            for td in tdlist:
-                if td != ' ':
-                    cell = ''
-                    for result in td.find_all('span'):
-                        if str(result.string) != 'None' and str(result.string) != '#':
-                            cell += str(result.string).replace(' ', '').replace('　', '').strip()
-                    line.append(cell)
-            #print(' : '.join(line))
-            page.append(line)
+        data = urllib.request.urlopen(target_url)
+        datastr = data.read().decode(encoding='utf-8')
+        soup = BeautifulSoup(datastr, 'lxml')
+        table = soup.find(class_ = 'MsoNormalTable')
+        tbody = table.tbody.contents
+        #print(tbody)
 
-    # output
-    with open('../data/' + year + '_' + str(month) + '_electricity.csv', 'w', newline='') as datacsv:
-        csvwriter = csv.writer(datacsv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        for line in page:
-            csvwriter.writerow(line)
+        #parse data
+        for tr in tbody:
+            if tr != ' ':
+                tdlist = tr.contents
+                line = []
+                for td in tdlist:
+                    if td != ' ':
+                        cell = ''
+                        for result in td.find_all('span'):
+                            if str(result.string) != 'None' and str(result.string) != '#':
+                                cell += str(result.string).replace(' ', '').replace('　', '').strip()
+                        line.append(cell)
+                #print(' : '.join(line))
+                if line[0].endswith('市') and line[0] not in ['昆山市', '泰兴市', '沭阳县']:
+                    monthdata.append(line[1])
+        yeardata.append(monthdata)
+        #print(len(monthdata))
+        #print(len(yeardata))
 
-    month += 1
+    yeardata.insert(0, yeardata[10])#insert fake data for Jan
+    #print(len(yeardata))
+    #print(len(yeardata[0]))
+
+    # output monthly electricity json
+    f = open('../data/' + year + '_electricity.json', 'w')
+    f.write(indentation(0) + '{\n')
+    f.write(indentation(1) + '"monthlydata" : [\n')
+    for month in range(len(month_name)):
+        if month != len(month_name) - 1 :
+            f.write(indentation(2) + '[' + ','.join(yeardata[month]) + '],\n')
+        else:
+            f.write(indentation(2) + '[' + ','.join(yeardata[month]) + ']\n')
+    f.write(indentation(1) + ']\n')
+    f.write(indentation(0) + '}\n')
+    f.close()
+
+    # output map electricity json
+    f = open('../data/' + year + '_map_electricity.json', 'w')
+    f.write(indentation(0) + '{\n')
+    f.write(indentation(1) + '"electricity" : [\n')
+    for month in range(len(month_name)):
+        f.write(indentation(2) + '{\n')
+        f.write(indentation(3) + '"month" : [\n')
+        for city in range(len(city_name)):
+            f.write(indentation(4) + '{')
+            f.write(indentation(0) + '"name" : "' + city_name[city] + '", ')
+            f.write(indentation(0) + '"value" : ' + str(yeardata[month][city]) + '')
+            if city == (len(city_name) - 1):
+                f.write(indentation(0) + '}\n')
+            else:
+                f.write(indentation(0) + '},\n')
+
+        f.write(indentation(3) + ']\n')
+
+        if month  == (len(month_name)-1):
+            f.write(indentation(2) + '}\n')
+        else:
+            f.write(indentation(2) + '},\n')
+
+    f.write(indentation(1) + ']\n')
+    f.write(indentation(0) + '}\n')
+    f.close()
+
+def indentation(number):
+    space = '    '
+    return '' + space * number
+
+if __name__ == '__main__':
+    main()
