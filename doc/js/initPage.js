1a5489df3013dbd86d25aed725dda8903ebe574e
youngspring1 <youngspring1@163.com>
2017-07-05 14:19:40 +0800

    fin homepage

---
+++ doc/js/initPage.js
@@ -0,0 +1,283 @@
+
+
+function initPage() {
+    initMap(2);
+    initDepositloan();
+}
+
+function initDepositloan() {
+    // 基于准备好的dom,初始化echarts实例
+    var timelineChart = echarts.init(document.getElementById('timelinediv'), 'dark');
+    // timelineChart.showLoading();
+
+    var option = {
+        baseOption: {
+            timeline: {
+                axisType: 'category',
+                show: true,
+                autoPlay: true,
+                playInterval: 1500,
+                controlPosition: 'right',
+                data: ['2016-01', '2016-02', '2016-03', '2016-04', '2016-05', '2016-06', '2016-07', '2016-08', '2016-09', '2016-10', '2016-11', '2016-12']
+            },
+            grid: {
+                containLabel: true
+            },
+            xAxis: {  
+                    type: 'category',
+                    // name: '地级市',
+                    data: ['南京', '无锡', '徐州', '常州', '苏州', '南通', '连云港', '淮安', '盐城', '扬州', '镇江', '泰州', '宿迁']
+            },  
+            yAxis: { 
+                    type: 'value', 
+                    // name: '亿元'
+                    max: 30000
+            },
+            series: [
+                {
+                    name: '存款',
+                    type: 'bar'
+                },
+                {
+                    name: '贷款',
+                    type: 'bar'
+                }
+            ],
+            tooltip: {
+                trigger: 'item',
+                formatter: '{b}<br/>{c} (亿元)'
+            },
+            legend: {
+                x: 'right',
+                data: ['存款', '贷款']
+            },
+            title: {
+                text: '江苏省各市存贷款',
+                // subtext: '江苏省统计局',
+                // sublink: 'http://www.jssb.gov.cn/tjxxgk/tjsj/jdsj/2016/'
+            }
+        },
+        options:[]
+    };
+    // set option before read json data
+    timelineChart.setOption(option);
+
+    $.get('../data/2016_depositloan.json').done(function (data) {
+        // console.log(data.monthlydata);
+        // read json data
+        var optiondata = {
+            options:[
+                {
+                    series: [
+                        {
+                            data : data.depositloan[0].deposit
+                        },
+                        {
+                            data : data.depositloan[0].loan
+                        }
+                    ]
+                },
+                {
+                    series: [
+                        {
+                            data : data.depositloan[1].deposit
+                        },
+                        {
+                            data : data.depositloan[1].loan
+                        }
+
+                    ]
+                },
+                {
+                    series: [
+                        {
+                            data : data.depositloan[2].deposit
+                        },
+                        {
+                            data : data.depositloan[2].loan
+                        }
+
+                    ]
+                },
+                {
+                    series: [
+                        {
+                            data : data.depositloan[3].deposit
+                        },
+                        {
+                            data : data.depositloan[3].loan
+                        }
+
+                    ]
+                },
+                {
+                    series: [
+                        {
+                            data : data.depositloan[4].deposit
+                        },
+                        {
+                            data : data.depositloan[4].loan
+                        }
+
+                    ]
+                },
+                {
+                    series: [
+                        {
+                            data : data.depositloan[5].deposit
+                        },
+                        {
+                            data : data.depositloan[5].loan
+                        }
+                    ]
+                },
+                {
+                    series: [
+                        {
+                            data : data.depositloan[6].deposit
+                        },
+                        {
+                            data : data.depositloan[6].loan
+                        }
+                    ]
+                },
+                {
+                    series: [
+                        {
+                            data : data.depositloan[7].deposit
+                        },
+                        {
+                            data : data.depositloan[7].loan
+                        }
+                    ]
+                },
+                {
+                    series: [
+                        {
+                            data : data.depositloan[8].deposit
+                        },
+                        {
+                            data : data.depositloan[8].loan
+                        }
+                    ]
+                },
+                {
+                    series: [
+                        {
+                            data : data.depositloan[9].deposit
+                        },
+                        {
+                            data : data.depositloan[9].loan
+                        }
+                    ]
+                },
+                {
+                    series: [
+                        {
+                            data : data.depositloan[10].deposit
+                        },
+                        {
+                            data : data.depositloan[10].loan
+                        }
+                    ]
+                },
+                {
+                    series: [
+                        {
+                            data : data.depositloan[11].deposit
+                        },
+                        {
+                            data : data.depositloan[11].loan
+                        }
+                    ]
+                }
+            ]
+        };
+        //set data
+        timelineChart.setOption(optiondata);
+    });
+}
+
+function initMap(mon) {
+    // 基于准备好的dom，初始化echarts实例
+    var myMapChart = echarts.init(document.getElementById('jsmap'), 'dark');
+
+    // load map
+    $.get('./map/jiangsu.json', function (geoJson) {
+        myMapChart.hideLoading();
+        echarts.registerMap('jiangsu', geoJson);
+    });
+
+    // load data
+    $.get('../data/2016_map_electricity.json').done(function (data) {
+        // console.log('in get json')
+        // console.log(data.electricity)
+        // console.log('electricity[1]:' + data.electricity[1].month[1])
+        // console.log('electricity[1].month[1].name:' + data.electricity[1].month[1].name)
+        // console.log('electricity[1].month[1].value:' + data.electricity[1].month[1].value)
+        // console.log('month = ' + mon);
+        // console.log(data.electricity[mon].month);
+
+        // 填入数据
+        myMapChart.setOption({
+            title: {
+                text: '江苏省各市每月用电量',
+                // subtext: '江苏省统计局',
+                sublink: 'http://www.jssb.gov.cn/tjxxgk/tjsj/jdsj/2016/'
+            },
+            tooltip: {
+                trigger: 'item',
+                formatter: '{b}<br/>{c} (亿千瓦小时)'
+            },
+            toolbox: {
+                show: false,//true,
+                orient: 'vertical',
+                left: 'right',
+                top: 'center',
+                feature: {
+                    dataView: {readOnly: false},
+                    restore: {},
+                    saveAsImage: {}
+                }
+            },
+            visualMap: {
+                min: 0,
+                max: 120,
+                text:['High','Low'],
+                realtime: false,
+                calculable: true,
+                inRange: {
+                    color: ['lightskyblue','yellow', 'orangered']
+                    //color: ['#ffde33', '#ff9933', '#cc0033', '#660099']
+                }
+            },
+            series: [{
+                name: '江苏省各市每月用电量',
+                type: 'map',
+                mapType: 'jiangsu', // 自定义扩展图表类型
+                itemStyle:{
+                    normal:{label:{show:true}},
+                    emphasis:{label:{show:true}}
+                },
+                data: data.electricity[mon].month,
+                nameMap: {
+                    //map data: display name
+                    '南京市': '南京市',
+                    '无锡市': '无锡市',
+                    '徐州市': '徐州市',
+                    '常州市': '常州市',
+                    '苏州市': '苏州市',
+                    '南通市': '南通市',
+                    '连云港市': '连云港市',
+                    '淮安市': '淮安市',
+                    '盐城市': '盐城市',
+                    '扬州市': '扬州市',
+                    '镇江市': '镇江市',
+                    '泰州市': '泰州市',
+                    '宿迁市': '宿迁市'
+                }
+            }]
+        });
+    });
+}
+
